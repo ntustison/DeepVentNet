352a6f00f75d62afd6ff2596e99cd1d4cb5a9ec8
ntustison <ntustison@gmail.com>
2018-04-20 15:27:29 -0700

    ENH:  INitial commit.

---
+++ Manuscript/stitchManuscript.R
@@ -0,0 +1,24 @@
+library( rmarkdown )
+
+stitchedFile <- "stitched.Rmd";
+
+formatFile <- "format.Rmd"
+
+rmdFiles <- c( formatFile,
+               "titlePage.Rmd",
+               "abstract.Rmd"
+   )
+
+for( i in 1:length( rmdFiles ) )
+  {
+  if( i == 1 )
+    {
+    cmd <- paste( "cat", rmdFiles[i], ">", stitchedFile )
+    } else {
+    cmd <- paste( "cat", rmdFiles[i], ">>", stitchedFile )
+    }
+  system( cmd )
+  }
+
+cat( '\n Pandoc rendering', stitchedFile, '\n' )
+render( stitchedFile, output_format = "pdf_document" )
