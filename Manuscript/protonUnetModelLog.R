f702724126ac0dcb20252480a89fe9d7c584e5ce
ntustison <ntustison@gmail.com>
2018-05-18 21:54:57 -0700

    ENH:  Getting closer.

---
+++ Manuscript/protonUnetModelLog.R
@@ -0,0 +1,22 @@
+library( ggplot2 )
+library( reshape2 )
+
+directory <- '/Users/ntustison/Documents/Academic/InProgress/DeepVentNet/Manuscript/'
+
+logUnet <- read.csv( paste0( directory, "protonUnetModelLog.csv" ) )
+logUnetLong <- melt( logUnet, id.vars = c( "Epoch" ), variable.name = "Data", 
+  value.name = "Dice" )
+
+unetModelAccuracyPlot <- 
+  ggplot( data = logUnetLong, aes( x = Epoch, y = Dice, colour = Data ) ) +
+  geom_point( shape = 1, size = 1.0 ) +
+  geom_line( size = 0.75 ) +
+  ggtitle( "Proton U-net model training" )
+
+ggsave( paste0( directory, "Figures/unetModelAccuracyPlot.pdf" ), 
+  plot = unetModelAccuracyPlot, width = 8, height = 3.5, units = 'in' )
+
+
+
+
+
---
+++ Scripts/Proton/parseLogFile.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl -w
+
+##
+## sample usage:  perl convertToNifti.pl subjects.txt Dicom Nifti
+##
+##
+
+
+# use module
+use XML::Simple;
+use Data::Dumper;
+use Cwd 'realpath';
+use Cwd;
+use File::Spec;
+use File::Find;
+use File::Basename;
+use File::Path;
+
+my $logFile = '/Users/ntustison/Documents/Academic/InProgress/DeepVentNet/Scripts/Proton/protonUnetModelLog.txt';
+
+open( FILE, "<$logFile" );
+my @contents = <FILE>;
+close( FILE );
+
+my $csvFile = '/Users/ntustison/Documents/Academic/InProgress/DeepVentNet/Manuscript/protonUnetModelLog.csv';
+
+open( FILE2, ">$csvFile" );
+print FILE2 "Epoch,Dice,Val_Dice\n";
+
+my $count = 1;
+for( my $i = 0; $i < @contents; $i++ )
+  {
+  if( $contents[$i] =~ m/^16\/16/ )
+    {
+    my @tokens = split( ' - ', $contents[$i] );
+    ( my $dice = ${tokens[2]} ) =~ s/loss:\ -//;
+    ( my $val_dice = ${tokens[4]} ) =~ s/val_loss:\ -//;
+
+    print FILE2 "$count,$dice,$val_dice\n";
+
+    $count++;
+    }
+  }
+
+close( FILE2 )  
